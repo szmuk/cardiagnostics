ea5b9efdcd9e356969249be9f4dcd0e372adfce6
szmuk <adamszmuk@gmail.com>
2016-01-13 22:32:38 +0100

    Added java-obd-api
    Signed-off-by: szmuk <adamszmuk@gmail.com>

--- app/src/main/java/com/example/szmuk/cardiagnostics/ObdInterface.java
+++ app/src/main/java/com/example/szmuk/cardiagnostics/ObdInterface.java
@@ -1,5 +1,8 @@
 package com.example.szmuk.cardiagnostics;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,9 +12,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.UUID;
+
 public class ObdInterface extends AppCompatActivity
 {
     Button b;
+
+    BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,24 +27,45 @@ public class ObdInterface extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        b = (Button) findViewById(R.id.cokolwiek);
-
         final Bundle bundle = getIntent().getExtras();
 
-        View.OnClickListener l = new View.OnClickListener()
+        BluetoothDevice device = btAdapter.getRemoteDevice(bundle.getString("address"));
+
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+        try
+        {
+            BluetoothSocket socket = device.createInsecureRfcommSocketToServiceRecord(uuid);
+
+            socket.connect();
+        }
+        catch(Exception ex)
+        {
+            Toast.makeText(getBaseContext(), "Failed to connect to: " + bundle.getString("address"), Toast.LENGTH_SHORT).show();
+        }
+
+
+
+        new EchoOffObdCommand().run(socket.getInputStream(), socket.getOutputStream());
+
+        new LineFeedOffObdCommand().run(socket.getInputStream(), socket.getOutputStream());
+
+        new TimeoutObdCommand().run(socket.getInputStream(), socket.getOutputStream());
+
+        new SelectProtocolObdCommand(ObdProtocols.AUTO).run(socket.getInputStream(), socket.getOutputStream());
+
+
+
+        b = (Button) findViewById(R.id.cokolwiek);
+
+        b.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
                 Toast.makeText(getBaseContext(), bundle.getString("address"), Toast.LENGTH_SHORT).show();
             }
-        };
-
-
-        b.setOnClickListener(l);
-
-
-
+        });
     }
 
 }
